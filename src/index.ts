9fc44b88a42515801d2f60603cf2d2cc6a72f933
Nafis Zaman <nafzam@outlook.com>
2024-08-16 11:22:11 -0700

    Run function every 5 mins for testing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { onSchedule } from "firebase-functions/v2/scheduler";
 import * as logger from "firebase-functions/logger";
 
-export const testFunction2 = onSchedule("every hour", async () => {
+export const testFunction2 = onSchedule("every 5 mins", async () => {
   logger.info("Hello functions 2!", { structuredData: true });
 });
