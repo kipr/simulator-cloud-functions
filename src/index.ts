16b7fbe91cf11e27c95dba82a8a92c9135b0bf43
Nafis Zaman <nafzam@outlook.com>
2024-08-16 09:44:00 -0700

    Update cd and config to fix deployment issues

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { onSchedule } from "firebase-functions/v2/scheduler";
 import * as logger from "firebase-functions/logger";
 
-export const unconsentedUserCleanup = onSchedule("every hour", async (event) => {
-  logger.info("Hello functions!", { structuredData: true });
+export const testFunction2 = onSchedule("every hour", async () => {
+  logger.info("Hello functions 2!", { structuredData: true });
 });
