232feb7b379212dc1578c23fa9491a1b2f097928
Nafis Zaman <nafzam@outlook.com>
2024-08-16 14:33:22 -0700

    Code bump for debugging

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { onSchedule } from "firebase-functions/v2/scheduler";
 import * as logger from "firebase-functions/logger";
 
 export const unconsentedUserCleanup = onSchedule("every 5 mins", async () => {
-  logger.info("Hello from function unconsentedUserCleanup 3!", { structuredData: true });
+  logger.info("Hello from function unconsentedUserCleanup 4!", { structuredData: true });
 });
