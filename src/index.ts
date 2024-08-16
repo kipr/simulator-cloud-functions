c1bf7f5861e3367cc2a7060052918d774d1203fe
Nafis Zaman <nafzam@outlook.com>
2024-08-16 12:48:36 -0700

    Add debug flag for deploy

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { onSchedule } from "firebase-functions/v2/scheduler";
 import * as logger from "firebase-functions/logger";
 
 export const unconsentedUserCleanup = onSchedule("every 5 mins", async () => {
-  logger.info("Hello from auto-deployed function unconsentedUserCleanup!", { structuredData: true });
+  logger.info("Hello from function unconsentedUserCleanup 2!", { structuredData: true });
 });
